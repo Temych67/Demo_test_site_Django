5c459967f3d628c4dc30aa18a460150466b969c3
Artem <voloshko9361@gmail.com>
2022-01-12 19:01:34 +0200

    updated settings ALLOWED_HOSTS variables

--- simple_test_site/simple_test_site/settings.py
+++ simple_test_site/simple_test_site/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['demotestsiteondjango.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['demotestsiteondjango.herokuapp.com', '127.0.0.1', '*']
 
 # Application definition
 
