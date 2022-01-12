d848171edd8875e99796f80239711889098b59d8
Artem <voloshko9361@gmail.com>
2022-01-12 19:46:13 +0200

    disable DEBUG

--- simple_test_site/simple_test_site/settings.py
+++ simple_test_site/simple_test_site/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['demotestsiteondjango.herokuapp.com', '127.0.0.1']
 
